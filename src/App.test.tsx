4f09528bde38a9a9026119a31222330412b20ade
Marlon A <maespinosa@dynamite.ca>
2022-02-27 17:15:35 -0500

    Fixed Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('Renders APP component', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Algo/);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/App/)
+  expect(linkElement).toBeInTheDocument()
+})
