c7adefa29b903e4e2b71b2fe3088efc7d3357536
Marlon A <maespinosa@dynamite.ca>
2022-02-27 16:57:46 -0500

    Added eslint and circleci config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./App.scss";
+import React from 'react';
+import './App.scss';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return <div className="App">App</div>;
+};
 
 export default App;
